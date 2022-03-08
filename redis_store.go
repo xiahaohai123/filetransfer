14161cd1d04fe55b8b57a4367d3bda1dcefc0104
summer_sea <xiahaohai@qq.com>
2022-03-08 18:30:54 +0800

    引入redis

--- redis_store.go
+++ redis_store.go
@@ -1,16 +1,22 @@
 package filetransfer
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/go-redis/redis"
 	"log"
+	"strings"
+	"time"
 )
 
-type RedisStore struct {
+const uploadSuffix = "upload"
+const downloadSuffix = "download"
+
+type redisStore struct {
 	client *redis.Client
 }
 
-func NewRedisStore(addr, password string, db int) (*RedisStore, error) {
+func NewRedisStore(addr, password string, db int) (DataStore, error) {
 	client := redis.NewClient(&redis.Options{
 		Addr:     addr,
 		Password: password,
@@ -21,29 +27,100 @@ func NewRedisStore(addr, password string, db int) (*RedisStore, error) {
 	if err != nil {
 		return nil, fmt.Errorf("problem connect to redis: %v", err)
 	}
-	return &RedisStore{client}, nil
+	return &redisStore{client}, nil
 }
 
-func (r RedisStore) SaveUploadData(taskId string, data UploadData) {
-	panic("implement me")
+func (r redisStore) SaveUploadData(taskId string, data UploadData) {
+	if taskId == "" {
+		return
+	}
+	key := r.createUploadKey(taskId)
+	uploadJSONData := r.data2Json(data)
+	r.client.Set(key, uploadJSONData, 10*time.Minute)
 }
 
-func (r RedisStore) GetUploadDataRemove(taskId string) *UploadData {
-	panic("implement me")
+func (r redisStore) GetUploadDataRemove(taskId string) *UploadData {
+	key := r.createUploadKey(taskId)
+	uploadJSONData, err := r.client.Get(key).Result()
+	if err == redis.Nil {
+		return nil
+	} else if err != nil {
+		log.Printf("problem get data: %v", err)
+		return nil
+	}
+	var uploadData UploadData
+	err = json.NewDecoder(strings.NewReader(uploadJSONData)).Decode(&uploadData)
+	if err != nil {
+		log.Printf("problem decode data: %v", err)
+	}
+	r.client.Del(key)
+	return &uploadData
 }
 
-func (r RedisStore) IsUploadTaskExist(taskId string) bool {
-	panic("implement me")
+func (r redisStore) IsUploadTaskExist(taskId string) bool {
+	_, err := r.client.Get(r.createUploadKey(taskId)).Result()
+	if err == redis.Nil {
+		return false
+	} else if err != nil {
+		log.Printf("problem get data: %v", err)
+		return false
+	}
+	return true
 }
 
-func (r RedisStore) SaveDownloadData(taskId string, data DownloadData) {
-	panic("implement me")
+func (r redisStore) SaveDownloadData(taskId string, data DownloadData) {
+	if taskId == "" {
+		return
+	}
+	key := r.createDownloadKey(taskId)
+	downloadJSONData := r.data2Json(data)
+	r.client.Set(key, downloadJSONData, 10*time.Minute)
 }
 
-func (r RedisStore) GetDownloadDataRemove(taskId string) *DownloadData {
-	panic("implement me")
+func (r redisStore) GetDownloadDataRemove(taskId string) *DownloadData {
+	key := r.createDownloadKey(taskId)
+	downloadJSONData, err := r.client.Get(key).Result()
+	if err == redis.Nil {
+		return nil
+	} else if err != nil {
+		log.Printf("problem get data: %v", err)
+		return nil
+	}
+	var downloadData DownloadData
+	err = json.NewDecoder(strings.NewReader(downloadJSONData)).Decode(&downloadData)
+	if err != nil {
+		log.Printf("problem decode data: %v", err)
+	}
+	r.client.Del(key)
+	return &downloadData
 }
 
-func (r RedisStore) IsDownloadTaskExist(taskId string) bool {
-	panic("implement me")
+func (r redisStore) IsDownloadTaskExist(taskId string) bool {
+	_, err := r.client.Get(r.createDownloadKey(taskId)).Result()
+	if err == redis.Nil {
+		return false
+	} else if err != nil {
+		log.Printf("problem get data: %v", err)
+		return false
+	}
+	return true
+}
+
+// 合成上传任务的key
+func (redisStore) createUploadKey(taskId string) string {
+	return fmt.Sprintf("%s:%s", uploadSuffix, taskId)
+}
+
+// 合成下载任务的key
+func (redisStore) createDownloadKey(taskId string) string {
+	return fmt.Sprintf("%s:%s", downloadSuffix, taskId)
+}
+
+// po转换成json
+func (r redisStore) data2Json(data interface{}) string {
+	bytes, err := json.Marshal(data)
+	if err != nil {
+		log.Printf("problem encode upload data to json")
+	}
+	return string(bytes)
 }
